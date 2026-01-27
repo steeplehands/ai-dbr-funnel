cf83fff4ffee29730b45e2cb6082763dec36c2bf
steeplehands <noahtaylor90@gmail.com>
2026-01-26 23:10:18 -0800

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/funnel1/',
   plugins: [
     // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
+    // Tailwind is not being actively used - do not remove them
     react(),
     tailwindcss(),
   ],
